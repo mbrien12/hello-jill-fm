df25f2ee73e0a0e180c284954d0cc93472ad571a
Michelle Brien <michelle.brien12@gmail.com>
2019-09-21 15:08:57 +0100

    updates css and adds new mixcloud methods

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,71 @@
-import React from "react";
+/*global Mixcloud*/
+import React, { Component } from "react";
 import FeaturedMix from "./FeaturedMix";
 import Header from "./Header";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      <div>
-        <FeaturedMix />
+const Home = () => <h1>Home</h1>;
+const Archive = () => <h1>Archive</h1>;
+const About = () => <h1>About</h1>;
+
+class App extends Component {
+  mountAudio = async () => {
+    this.widget = Mixcloud.PlayerWidget(this.player);
+    await this.widget.ready;
+    await this.widget.play();
+  };
+
+  componentDidMount() {
+    this.mountAudio();
+  }
+
+  togglePlay = () => {
+    this.widget.togglePlay();
+  };
+
+  playMix = mixName => {
+    this.widget.load(mixName, true);
+  };
+
+  render() {
+    return (
+      <Router>
         <div>
-          <Header />
-          {/* RoutedPages */}
+          <div className="flex-l justify-end">
+            <FeaturedMix />
+            <div className="w-50-l relative z-1">
+              <Header />
+              <div>
+                <button onClick={this.togglePlay}>Play/Pause</button>
+              </div>
+              <div>
+                <button
+                  onClick={() =>
+                    this.playMix(
+                      "/Mr_Scruff/mr-scruff-with-mc-kwasi-dj-set-keep-it-unreal-20th-birthday-manchester-2019/"
+                    )
+                  }
+                >
+                  Play mix
+                </button>
+              </div>
+              <Route exact path="/" component={Home} />
+              <Route path="/archive" component={Archive} />
+              <Route path="/about" component={About} />
+            </div>
+          </div>
+          <iframe
+            width="100%"
+            height="60"
+            src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Ftheheatwave%2Fcarnival-bashment-2016%2F"
+            frameBorder="0"
+            className="db fixed bottom-0 z-5"
+            ref={player => (this.player = player)}
+          />
         </div>
-      </div>
-      <iframe
-        width="100%"
-        height="60"
-        src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Ftheheatwave%2Fcarnival-bashment-2016%2F"
-        frameBorder="0"
-      />
-    </div>
-  );
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = props => (
-  <header>
-    <h1>Hello Jill FM</h1>
-    <ul>
-      <li>What's hot</li>
-      <li>Archive</li>
-      <li>About</li>
+  <header className="black mb5 pt5">
+    <h1 className="ttu f3 tracked-mega anton tc mt0 mb3">Hello Jill FM</h1>
+    <ul className="list flex justify-center pl0">
+      <li className="mh2">
+        <NavLink exact to="/" className="nav-link link biryani-black f6 ttu gray">
+          What's hot
+        </NavLink>
+      </li>
+      <li className="mh2">
+        <NavLink to="/archive" className="nav-link link biryani-black f6 ttu gray">
+          Archive
+        </NavLink>
+      </li>
+      <li className="mh2">
+        <NavLink to="/about" className="nav-link link biryani-black f6 ttu gray">
+          About
+        </NavLink>
+      </li>
     </ul>
   </header>
-)
+);
 
-export default Header
+export default Header;
