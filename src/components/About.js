608210f2b4fd233565d251b347e4b164147e719c
Michelle Brien <michelle.brien12@gmail.com>
2020-03-28 19:00:05 +0000

    adds counter component

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,44 @@
 import React from "react";
+import Counter from "./Counter";
 
 const Stat = ({ statName, statNumber, statWord }) => (
-  <div
-    className="w-third tc pa3 ba bw2 b--light-gray"
-    style={{ marginRight: -2 }}
-  >
-    <div className="f6 biryani ttu">{statName}</div>
-    <div className="f5 b biryani-black ttu tracked">
-      {statNumber}
-      {statWord}
-    </div>
+  <div className="mb4" style={{ marginRight: -2 }}>
+    <div className="f5 black mb0 b">{statName}</div>
+    <Counter end={statNumber} duration={2} />
+    <div className="f4 lh-1">{statWord}</div>
   </div>
 );
 
-const About = ({mixes}) => (
-  <div className="ph3 ph4-1">
-    <div className="measure center 1h-copy f4 ph3">
+const About = ({ mixes }) => (
+  <div className="measure center">
+    <div className="1h-copy mb4">
       <p className="mt0">
         Hello Jill FM brings you banging tunes for banging days.
       </p>
       <p>Rock your socks off.</p>
     </div>
-    <div className="flex pt3">
-      <Stat statName="Featuring..." statNumber={mixes.length} statWord=" mixes" />
-      <Stat statName="Played..." statNumber={mixes.reduce((accum, current) => accum + current.play_count, 0)} statWord=" times" />
-      <Stat statName="With..." statNumber={mixes.reduce((accum, current) => accum + current.audio_length, 0)} statWord=" mins" />
+    <div>
+      <Stat
+        statName="Featuring..."
+        statNumber={mixes.length}
+        statWord=" mixes"
+      />
+      <Stat
+        statName="Played..."
+        statNumber={mixes.reduce(
+          (accum, current) => accum + current.play_count,
+          0
+        )}
+        statWord=" times"
+      />
+      <Stat
+        statName="With..."
+        statNumber={mixes.reduce(
+          (accum, current) => accum + current.audio_length,
+          0
+        )}
+        statWord=" mins"
+      />
     </div>
   </div>
 );
