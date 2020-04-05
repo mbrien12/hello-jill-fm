2307739952acd7455e6a072d2714070ba64b9851
Michelle Brien <michelle.brien12@gmail.com>
2020-04-05 19:47:51 +0100

    fixes Effect and added stats to show page

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Stat from "./Stat";
 
 const Show = ({ match, mixes }) => {
   const [mix, setMix] = useState(null);
@@ -10,7 +11,22 @@ const Show = ({ match, mixes }) => {
   return (
     <div className="ph3 ph4-1 pad-bottom">
       <div className="measure center 1h-copy">
-        <p>{mix.description}</p>
+        <p>{mix && mix.description}</p>
+        <Stat
+          statName="Plays"
+          statNumber={mix && mix.play_count}
+          statWord=" times"
+        />
+        <Stat
+          statName="Favourited"
+          statNumber={mix && mix.favorite_count}
+          statWord="times"
+        />
+        <Stat
+          statName="Lasting for"
+          statNumber={(mix && mix.audio_length) / 60}
+          statWord="minutes"
+        />
       </div>
     </div>
   );
