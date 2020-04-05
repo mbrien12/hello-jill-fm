92edc00d65026c760af569f7ec12a2a35df59bbe
Michelle Brien <michelle.brien12@gmail.com>
2020-04-05 14:47:01 +0100

    adds Effect - broken need to get working!

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Show = ({ match }) => (
-  <div>
-    <h1>Show page </h1>
-    <p>{match.params.slug}</p>
-  </div>
-);
+const Show = ({ match, mixes }) => {
+  const [mix, setMix] = useState(null);
+  useEffect(() => {
+    const [firstMix = {}] = mixes.filter(mix => mix.slug === match.params.slug);
+    setMix(firstMix);
+  }, [mix]);
 
+  return (
+    <div className="ph3 ph4-1 pad-bottom">
+      <div className="measure center 1h-copy">
+        <p>{mix.description}</p>
+      </div>
+    </div>
+  );
+};
 export default Show;
